6fdc6769ae1a986da38b2c95780ee3403f66da96
Ca010 <mila_aandrade@hotmail.com>
2021-12-05 20:30:00 -0300

    AC3

---
+++ AC3/exibindo_tabuada.py
@@ -0,0 +1,10 @@
+a = int(input())
+b = int(input())
+
+for i in range(1, 11):
+    print('{} x {} = {}'.format(a , i , i * a))
+
+print('---------')
+for i in range(1, 11):
+    print('{} x {} = {}'.format(b , i , i * b))
+   
